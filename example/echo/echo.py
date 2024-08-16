6d99da7fa22a53807f702147e452f006e868b22d
Harry Askham <harryaskham@gmail.com>
2024-08-16 21:20:02 +0100

    [no commit message]

--- example/echo/echo.py
+++ example/echo/echo.py
@@ -1,5 +1,2 @@
 def run(request, name=None):
-    # Loads from either the free-text path or the request body
-    if not name:
-        name = request.path.split("/")[-1]
-    return f"Hello, {name}!"
+    return f"Hello, {name}, from {Server.sing().address}:{Server.sing().port}!"
