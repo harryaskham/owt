3431df30fb09e70130635fc97112428c9a61963e
Harry Askham <harryaskham@gmail.com>
2024-08-16 15:16:47 +0100

    Ersatz webserver running

--- example/bark/bark_construct_curl.sh
+++ example/bark/bark_construct_curl.sh
@@ -4,8 +4,11 @@ with open('example/bark/bark.html', 'r') as html_f:
   html = html_f.read()
   with open('example/bark/bark.py', 'r') as code_f:
     code = code_f.read()
-    template = html.replace('{% include "bark.py" %}', code)
-    print('''
+    with open('example/bark/bark.js', 'r') as js_f:
+      template = (html.replace('{% include "bark.py" %}', code)
+                  .replace('<script src="/bark/bark.js"></script>',
+                           '<script>\n'+js_f.read()+'\n</script>'))
+      print('''
 def run(_):
   from flask import render_template
   try:
