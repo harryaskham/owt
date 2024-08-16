72cb175912cc9a19928d3a5d7f0039bec7d1b835
Harry Askham <harryaskham@gmail.com>
2024-08-16 20:05:05 +0100

    Org readme

--- example/echo/echo_request.sh
+++ example/echo/echo_request.sh
@@ -1,3 +1,4 @@
 URL="$1"
 CODE_B64=$(cat example/echo/echo.py | base64 -w 0)
-curl $URL/owt?code_b64=$CODE_B64
+KWARGS_B64=$(echo "{'name': 'owt'}" | base64 -w 0)
+curl -G --data-urlencode code_b64=$CODE_B64 --data-urlencode kwargs_b64=$KWARGS_B64 $URL
