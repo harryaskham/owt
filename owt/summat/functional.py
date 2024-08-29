7b6e902ec5354f457acbbbc57ba2a8d3f54d13df
Harry Askham <harryaskham@gmail.com>
2024-08-29 10:04:37 +0100

    [rapid commit; squash later]

--- owt/summat/functional.py
+++ owt/summat/functional.py
@@ -1,6 +1,6 @@
 from owt.summat.adaptor import Adaptor, Out, L, HasLast, CallOut, KeepKWs, DropKWs, SetKWs, Passthrough
 
-from typing import Any, Callable, Sequence
+from typing import Any, Callable, Sequence, Unpack
 
 
 class F[**T, U](Adaptor[T, U]):
@@ -27,25 +27,25 @@ class Const[U](Adaptor[Any, U]):
     def __init__(self, a: U) -> None:
         self.a = a
 
-    def call(self, **_: Any) -> CallOut[T]:
+    def call(self, **_: Any) -> CallOut[U]:
         return DropKWs(self.a)
 
 
-class Identity[T](Adaptor[T, T]):
-    def __call__(self, **kwargs: In[T]) -> Out[T]:
-        return kwargs["__last__"], {}
+class Identity[T](Adaptor[HasLast[T], T]):
+    def call(self, **kwargs: HasLast[T]) -> CallOut[T]:
+        return Passthrough()
 
 
-class Cond[T, U, V](Adaptor[T, U | V]):
+class Cond[**T, U, V](Adaptor[T, U | V]):
     def __init__(self, _then: Adaptor[T, U], _else: Adaptor[T, V]) -> None:
         self._then = _then
         self._else = _else
 
-    def __call__(self, **kwargs: In[T]) -> Out[U | V]:
-        if "__last__" in kwargs and kwargs["__last__"]:
-            return self._then(**kwargs), {}
+    def call(self, **kwargs: T.kwargs) -> CallOut[U] | CallOut[V]:
+        if kwargs["__last__"]:
+            return self._then.call(**kwargs)
         else:
-            return self._else(**kwargs), {}
+            return self._else.call(**kwargs)
 
 
 class Fork[T, U, V](Adaptor[T, tuple[U, V]]):
@@ -53,10 +53,10 @@ class Fork[T, U, V](Adaptor[T, tuple[U, V]]):
         self.left = left
         self.right = right
 
-    def __call__(self, **kwargs: In[T]) -> Out[tuple[U, V]]:
-        left, lkwargs = self.left(**kwargs)
-        right, rkwargs = self.right(**kwargs)
-        return (left, right), {**lkwargs, **rkwargs}
+    def call(self, **kwargs: HasLast[T]) -> CallOut[tuple[U, V]]:
+        left_result = self.left.call(**kwargs)
+        right_result = self.right.call(**kwargs)
+        return SetKWs((left_result.value, right_result.value), {**left_result.kwargs, **right_result.kwargs})
 
 
 class Map[T, U](F[Sequence[T], Sequence[U]]):
@@ -69,31 +69,31 @@ class Foldl[T, U, V](Adaptor[T, V]):
         self.f = f
         self.initial = initial
 
-    def __call__(self, **kwargs: In[T]) -> Out[V]:
+    def call(self, **kwargs: HasLast[T]) -> CallOut[V]:
         sequence = kwargs["__last__"]
         result = self.initial
         for item in sequence:
             result = self.f(result, item)
-        return result, {}
+        return DropKWs(result)
 
 
 class Foldl1[T, V](Adaptor[T, V]):
     def __init__(self, f: Callable[[V, V], V]) -> None:
         self.f = f
 
-    def __call__(self, **kwargs: In[T]) -> Out[V]:
+    def call(self, **kwargs: HasLast[T]) -> CallOut[V]:
         sequence = kwargs["__last__"]
         if not sequence:
             raise ValueError("Sequence must not be empty for Foldl1")
         result = sequence[0]
         for item in sequence[1:]:
             result = self.f(result, item)
-        return result, {}
+        return DropKWs(result)
 
 
 class Ix[T](Adaptor[Sequence[T], T]):
     def __init__(self, i: int) -> None:
         self.i = i
 
-    def __call__(self, **kwargs: In[Sequence[T]]) -> Out[T]:
-        return kwargs["__last__"][self.i], {}
+    def call(self, **kwargs: HasLast[Sequence[T]]) -> CallOut[T]:
+        return DropKWs(kwargs["__last__"][self.i])
